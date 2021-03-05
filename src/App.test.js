a9c87d88f8670b288711f848166c72520ce0c508
Arun Ravishankar <aravishankar2@gmail.com>
2021-03-04 19:14:01 -0600

    swapped out jest for enzyme

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,17 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import App from "./App";
+import { shallow } from "enzyme";
+// test("renders learn react link", () => {
+//   render(<App />);
+//   const linkElement = screen.getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+//  We create a "suite" by using the word describe
+//      We pass in a label string, as well as a callback function to our suite
+describe("App", () => {
+  // We describe the behavior of the app componenent
+  // "It" also takes in a label and a callback, but this would be considered our first test.
+  it("", () => {
+    const appWrapper = shallow(<App />);
+  });
 });

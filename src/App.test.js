bd02e484d7e78ef9b0b3bc0b72b2e66bc890b1ac
Arun Ravishankar <aravishankar2@gmail.com>
2021-03-04 20:35:02 -0600

    make code DRY by using the 'beforeAll' function to declare variables prior to running your suite

--- src/App.test.js
+++ src/App.test.js
@@ -7,28 +7,27 @@ import { shallow } from "enzyme";
 describe("App", () => {
   // We describe the behavior of the app componenent
   // "It" also takes in a label and a callback, but this would be considered our first test.
-  it("renders App component successfully", () => {
-    const appWrapper = shallow(<App />);
+  // it("renders App component successfully", () => {
+  //   const appWrapper = shallow(<App />);
+  // });
+  let appWrapper;
+  let state;
+  beforeAll(() => {
+    appWrapper = shallow(<App />);
+    state = appWrapper.state();
   });
-
   it("Renders A Person List", () => {
-    const appWrapper = shallow(<App />);
     const personList = appWrapper.find(PersonList);
     //  We can check for the occurances of Person List within the App component as an array
     expect(personList).toHaveLength(1);
   });
 
   it("App Component Has State", () => {
-    const appWrapper = shallow(<App />);
-    const state = appWrapper.state();
-
     // We can write an assertion, to expect that our components state is or is not null
     expect(state).not.toBeNull();
   });
 
   it("State Has 'People' Property", () => {
-    const appWrapper = shallow(<App />);
-    const state = appWrapper.state();
     expect(state.people).toBeDefined();
   });
 });
